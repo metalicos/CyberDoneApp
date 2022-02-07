c0897255b389dc4a850ad0adf4672d0b401f4f93
Ostap Komplikevych <ostap.ja@gmail.com>
2022-02-07 23:35:16 +0200

    decomposed service

---
+++ src/app/services/device-metadata.service.ts
@@ -0,0 +1,93 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {SERVER} from '../backend-urls';
+
+export interface DeviceMetadataDto {
+  id: number;
+  uuid: string;
+  name: string;
+  description: string;
+  deviceType: string;
+  accessEnabled: boolean;
+  userId: number | undefined;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeviceMetadataService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getMetadataByUuid(uuid: string, value: string) {
+    return this.http.get<DeviceMetadataDto>(SERVER.backendServerUrl + '/device/metadata',
+      {
+        headers: SERVER.HEADERS,
+        params: new HttpParams().set('uuid', uuid).set('value', value)
+      }
+    );
+  }
+
+  getMetadataListByUser(userId: number) {
+    return this.http.get<DeviceMetadataDto[]>(SERVER.backendServerUrl + '/device/metadata/list',
+      {
+        headers: SERVER.HEADERS,
+        params: new HttpParams().set('userId', String(userId))
+      }
+    );
+  }
+
+  createMetadata(deviceMetadataDto: DeviceMetadataDto) {
+    return this.http.post(SERVER.backendServerUrl + '/device/metadata', deviceMetadataDto,
+      {
+        headers: SERVER.HEADERS,
+        params: new HttpParams()
+      }
+    );
+  }
+
+  updateMetadata(uuid: string, name: string, description: string) {
+    return this.http.patch(SERVER.backendServerUrl + '/device/metadata', {},
+      {
+        headers: SERVER.HEADERS,
+        params: new HttpParams().set('uuid', uuid).set('name', name).set('description', description)
+      }
+    );
+  }
+
+  deleteMetadata(uuid: string) {
+    return this.http.delete(SERVER.backendServerUrl + '/device/metadata',
+      {
+        headers: SERVER.HEADERS,
+        params: new HttpParams().set('uuid', uuid)
+      }
+    );
+  }
+
+  getDeviceTypesList() {
+    return this.http.get<string[]>(SERVER.backendServerUrl + '/device/metadata/device-types',
+      {
+        headers: SERVER.HEADERS,
+      }
+    );
+  }
+
+  unlinkDevice(uuid: string) {
+    return this.http.put(SERVER.backendServerUrl + '/device/metadata/unlink', {},
+      {
+        headers: SERVER.HEADERS,
+        params: new HttpParams().set('uuid', uuid)
+      }
+    );
+  }
+
+  linkDevice(uuid: string, userId: number) {
+    return this.http.put<string>(SERVER.backendServerUrl + '/device/metadata/link', {},
+      {
+        headers: SERVER.HEADERS,
+        params: new HttpParams().set('uuid', uuid).set('userId', String(userId))
+      }
+    );
+  }
+}
